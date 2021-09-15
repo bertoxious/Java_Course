afbc6f335e827660e128c1bf6b7cc57de62ed02f
Ashish <ashishdev@zohomail.in>
2021-09-15 12:22:42 +0530

    Constructor Overloading

---
+++ Practice/Constructors.java
@@ -0,0 +1,19 @@
+public class Constructors{
+	String name; int salary; String desi;
+
+	Constructors(String username, int usersalary){
+		name = username;
+		salary = usersalary;
+	}
+	Constructors(String username, int usersalary, String designation){
+		this(username, usersalary);
+		desi = designation;
+	}
+	public static void main(String [] args){
+		Constructors emp = new Constructors("Ashish", 15000);
+		System.out.printf("Name of the person is :: %s \n Salary of the person is :: %d \n Designation :: Employee ",emp.name, emp.salary);
+		Constructors boss = new Constructors("Mayank",65000, "Boss");
+		System.out.printf("Name of the person is :: %s \n Salary of the persons is :: %d \n Designation :: %s",boss.name, boss.salary,boss.desi);
+
+	}
+}
