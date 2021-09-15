ebac1151943cd6d2ab05d66fb8c1e7c4acb881dc
Ashish <ashishdev@zohomail.in>
2021-09-15 09:50:05 +0530

    Static Vs Non-Static

--- Practice/Calling.java
+++ Practice/Calling.java
@@ -1,15 +1,16 @@
 public class Calling{
-	
+	// Memory allocation for a static variable is only done once.
+	// for a non-static variable, memory is allocated every time an instance of the class is created.
 	static int demo = 45;
-
+	int retro = 56;
 	// Declaring static variable inside main function will generate compile 
 	// time error.
-	
+
 	void print_function(){
 		System.out.println("Simple print function.");
 	}
 
-	static void print_fun(){
+	static void print_fun(int a, int b , int c){
 		System.out.println("Static print function.");
 	}
 	public static void main(String [] args){
@@ -20,5 +21,11 @@ public class Calling{
 
 		demo = 46;
 		System.out.println(demo);
+
+		Calling.demo = 47;
+		System.out.println(demo);
+		// Static Variable can be accessed the same way as static function
+		c.retro = 97;
+		System.out.println(c.retro);
 	}
 }
