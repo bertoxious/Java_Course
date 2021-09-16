3733fa9e6530980a958ff681b1cfd090d6df1dd1
Ashish <ashishdev@zohomail.in>
2021-09-16 16:18:08 +0530

    Fibionacci Sequence

---
+++ Practice/Fibionacci.java
@@ -0,0 +1,22 @@
+public class Fibionacci{
+
+
+	static int Fibionacci(int number){
+		int a = 0; int b = 1;int sum;
+		if (number == 0){
+			return 0;
+		}
+		else if (number == 1){
+			return 1;
+		}
+		for (int i=0; i < number-2 ; i++){
+			sum = a+b;
+			a = b;
+			b = sum;
+		}
+		return b;
+	}
+	public static void main(String [] args){
+		System.out.println(Fibionacci(6));
+	}
+}
