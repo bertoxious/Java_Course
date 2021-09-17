8645b7c882be4ee4e4749ec10ade877ad385ed7d
Ashish <ashishdev@zohomail.in>
2021-09-17 15:43:48 +0530

    👉 Bit Shift 👈

--- Practice/BitShift.java
+++ Practice/BitShift.java
@@ -2,11 +2,25 @@ public class BitShift{
 	public static void main(String [] args){
 
 		// left shift
-		int x = 10 ;
-	    x = x << 1;
-		x++;
+		int x = -1;
+	    x = x << 5;
 		System.out.println(x);
-		System.out.println(x << 1);
+
+		// right shift
+		int y = -1;
+		y = y >> 5;
+		System.out.println(y);
+
+		// right unsigned shift
+		int z = -10;
+		z = z >>> 1;
+		System.out.println(z);
+
+		// No unsigned left shift in java
+
+		// int w = 0;
+		// w = w <<< 5;
+		// System.out.println(w);
 
 	}
 }
