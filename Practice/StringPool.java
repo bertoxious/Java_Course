2b67ee4218184a96d861266a05cbe3c2f9811401
Ashish <ashishdev@zohomail.in>
2021-09-20 12:48:19 +0530

    👉 String Pool 👈

---
+++ Practice/StringPool.java
@@ -0,0 +1,14 @@
+public class StringPool{
+	public static void main(String [] args){
+	// String Pool
+	String s = "Java";
+ 	// String Pool
+	String d = new String("Java").intern();
+	// Java Heap
+	String a = new String("Java");
+
+	System.out.println("With intern() :: " + (s==d));
+	System.out.println("Without intern() :: "+ (s==a));
+
+	}
+}
