364099121448563a3a7028b3d8fb5b40809fff7f
Ashish <ashishdev@zohomail.in>
2021-09-21 10:23:19 +0530

    👉 Polymorphism 👈

---
+++ Polymorphism/User.java
@@ -0,0 +1,16 @@
+public class User{
+
+	public void printUserType(){
+		System.out.println("User");
+	}
+	public void saveWebLink(){
+		System.out.println("User : SaveWebLink");
+		postAReview();
+	}
+	public void postAReview(){
+		System.out.println("Usesr : postAReview");
+	}
+	public static void main(String [] args){
+
+	}
+}
---
+++ Polymorphism/UserTest.java
@@ -0,0 +1,17 @@
+public class UserTest{
+	public void printUserType(User u){
+		u.printUserType();
+	}
+	public static void main(String [] args){
+		User user = new User();
+		User staff = new Staff();
+		User editor = new Editor();
+
+		UserTest ut = new UserTest();
+		ut.printUserType(user);
+		ut.printUserType(staff);
+		ut.printUserType(editor);
+		
+
+	}
+}
