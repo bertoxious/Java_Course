456293610875890c325ef4f5096fe4441969c6f8
Ashish <ashishdev@zohomail.in>
2021-09-28 10:40:03 +0530

    Bu Sh In Se

--- Sorting Algorithms/Sorting.java
+++ Sorting Algorithms/Sorting.java
@@ -2,7 +2,9 @@ import java.util.Arrays;
 
 public class Sorting{
 
-    // Bubble sort
+    // Bubble sort - Stable sorting algorithm
+    /* Bubble sort - swapping between the adjancent elements based on which
+    one is greater and which one is smaller*/
 	static int[] Bubble(int [] arr){
 
 		for(int i=0;i < arr.length;i++){
@@ -16,7 +18,10 @@ public class Sorting{
 		}
 		return arr;
 	}
-	// Insertion Sort
+	// Insertion Sort - Stable sorting algorithm
+	/* Insertion Sort- select one element at a time from the left of the collection.
+	Insert the element at proper position. 
+	After insertion every element to the left of it will be sorted.*/
 	static int[] Insertion(int [] arr){
 
 		for(int i= 0; i < arr.length-1; i++){
@@ -30,7 +35,9 @@ public class Sorting{
 		}
 		return arr;
 	}
-	// Selection Sort 
+	// Selection Sort - Unstable Sorting Algorithm
+	/* Selection sort - we select a particular max or min element 
+	and place it in its right position*/
 	static int[] Selection(int [] arr){
 
 		for(int i= arr.length-1; i >=0 ; i--){
@@ -45,10 +52,34 @@ public class Sorting{
 		return arr;
 	}
 
+	// Shell Sort - Unstable Sorting Algorithm
+	/* Selects an element and compares element after a gap (Where gap = length / 2)
+	Similar to Insertion Sort
+	Inserts selected element from the gap at its proper position*/
+	static int[] ShellSort(int [] arr){
+		int gap = arr.length / 2;
+		for (int i= 0; i < gap ; i ++){
+			if (arr[i] > arr[i+gap]){
+				int temp = arr[i];
+				arr[i] = arr[i+gap];
+				arr[i+gap] = temp;
+			}
+		}
+		return arr;
+	}
+	// Merge Sort - Stable Algorithm
+	/* Divides the collection of elements into smaller subsets.
+	Recursively Sorts the subsets
+	Combine or merge the result into a solution
+	Merger sort uses divide and conquer approach*/
+	static int[] MergeSort(int [] arr){
+		
+	}
 	public static void main(String args[]){
 		int [] sam = {60,50,40,30,20,10};
 		System.out.println(Arrays.toString(Bubble(sam)));
 		System.out.println(Arrays.toString(Insertion(sam)));
 		System.out.println(Arrays.toString(Selection(sam)));
+		System.out.println(Arrays.toString(ShellSort(sam)));
 	}
 }
