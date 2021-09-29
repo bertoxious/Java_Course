c0e53fa04b8e1882a83e1eaf343e1bf6784d696a
Ashish <ashishdev@zohomail.in>
2021-09-29 12:34:36 +0530

    Fixed

---
+++ Collections/ListInterfaceExample.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListInterfaceExample{
+
+	public static void main(String [] args){
+		List <String> ninja = new ArrayList<String>();
+		ninja.add("Naruto");
+		ninja.add("Sakura");
+		ninja.add("Sasuke");
+		
+		System.out.println(ninja);
+	}
+}
--- Practice/Factorial.java
+++ Practice/Factorial.java
@@ -4,7 +4,7 @@ public class Factorial{
 		if ( number == 1){
 			return number;
 		}
-		return number*(number-1);
+		return number*Factorial_method(number-1);
 	}
 	public static void main(String [] args){
 		System.out.println(Factorial_method(5));
