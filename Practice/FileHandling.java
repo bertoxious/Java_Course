37aac0c8cbf4f90cd70bfbef6a4b4f15ad584013
Ashish <ashishdev@zohomail.in>
2021-09-30 10:09:04 +0530

    👉 File Handling 👈

--- Practice/FileHandling.java
+++ Practice/FileHandling.java
@@ -21,11 +21,25 @@ public class FileHandling{
 		// Writing to a file 
 		try{
 			FileWriter fileWriter = new FileWriter(fileName);
-			fileWriter.write("Fabian Mazur & Snavs -> Arena \nRl Grime -> UCLA \nAero Chord X Woflgang Gartner -> Borneo");
+			fileWriter.write("Fabian Mazur & Snavs -> Arena 🎶 \nRl Grime -> UCLA 🎶 \nAero Chord X Woflgang Gartner -> Borneo 🎶");
 			fileWriter.close();	
 		}
 		catch(Exception e){
 			System.out.println("Error :: Unable to write to the file.");
+			e.printStackTrace();
+		}
+		// Reading a file 
+		try{
+			Scanner sf = new Scanner(myfile);
+			System.out.println("The file contains these lines");
+			while(sf.hasNextLine()){
+				String line = sf.nextLine();
+				System.out.println(line);
+			}
+			sf.close();
+		}
+		catch(Exception e){
+			System.out.println("Error :: Unable to read the file.");
 			e.printStackTrace();
 		}
 
