ccbd30c8aa2230d473eabb3771bb288564ab12e5
Ashish <ashishdev@zohomail.in>
2021-10-01 14:46:07 +0530

    Interface Casting

---
+++ Practice/InterfaceCasting.java
@@ -0,0 +1,28 @@
+class Papa{
+	void method(){
+		System.out.println("I am Papa");
+	}
+}
+
+interface Interface{
+	public void inside_interface();
+}
+class Beta extends Papa implements Interface{
+	public void inside_interface(){
+		System.out.println("Interface method");
+	}
+	public void beta_method(){
+		System.out.println("I am beta");
+	}
+
+} 
+
+public class InterfaceCasting{
+
+	public static void main(String [] args){
+		Interface inter = new Beta();
+		inter.method();
+		inter.beta_method();
+		inter.inside_interface();
+	}
+}
