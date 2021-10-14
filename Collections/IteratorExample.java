0d3df05a8e5df9a5127926cd836481031ca82f6a
Ashish <ashishdev@zohomail.in>
2021-10-14 10:52:33 +0530

    iterator Example

--- Collections/IteratorExample.java
+++ Collections/IteratorExample.java
@@ -1,12 +1,21 @@
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Iterator;
 
 public class IteratorExample{
 	public static void main(String [] args){
+		// Iterator
 		List<Integer> integerList = new LinkedList<Integer>();
 		integerList.add(34);
 		integerList.add(45);
 		integerList.add(56);
 		System.out.println(integerList);
+
+		Iterator<Integer> iterator = integerList.iterator();
+		System.out.println(iterator);
+		while(iterator.hasNext()){
+			int element = iterator.next();
+			System.out.println(element);
+		}
 	}
 }
