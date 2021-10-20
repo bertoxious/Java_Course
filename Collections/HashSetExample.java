9109e574a9ce5579cd3f784e87c51278ee2d96a1
Ashish <ashishdev@zohomail.in>
2021-10-20 11:13:39 +0530

    Hash Set

--- Collections/HashSetExample.java
+++ Collections/HashSetExample.java
@@ -1,18 +1,25 @@
 import java.util.HashSet;
 import java.util.Set;
 
-
-
 public class HashSetExample{
 	
 	public static void main(String [] args){
 		Set<String> sHash = new HashSet<>();
-		sHash.add("Amazon");
+
 		sHash.add("Amazon");
 		sHash.add("Google");
 		sHash.add("Tesla");
 		sHash.add("Microsoft");
 
 		System.out.println(sHash);
+		Set<Integer> iHash = new HashSet<>();
+
+		iHash.add(34);
+		iHash.add(12);
+		iHash.add(45);
+		iHash.add(78);
+
+		System.out.println(iHash);
+
 	}
 }
